b24e569fb3f5921cdb501053f13888bdd163ae10
azoz-moh <azozmomm11@gmail.com>
2022-03-17 15:19:34 +0300

    My Initial Commit in git

---
+++ main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+import pandas as pd
+from datetime import datetime
+import json
+
+app = FastAPI()
+
+Machine_date = datetime.today().strftime('%d-%m')
+
+
+@app.get('/')
+def to_pray():
+    df = pd.read_excel('Salah-Calendar.xlsx', sheet_name='Table 1', usecols="A:L")
+    for row in range(len(df)):
+        if df.loc[row]['Date'].strftime('%d-%m') == Machine_date:
+            adan = {
+                'Date': df.loc[row]['Date'].strftime('%d-%m'),
+                'Fajr': df.loc[row]['Fajr'],
+                'Fajr Iqama': df.loc[row]['Fajr Iqama'].strftime('%I:%M %p'),
+                'Sunrise': df.loc[row]['Sunrise'],
+                'Zuhr': df.loc[row]['Zuhr'],
+                'Asr': df.loc[row]['Asr'],
+                'Maghrib': df.loc[row]['Maghrib'],
+                'Isha': df.loc[row]['Isha'],
+            }
+            return json.dumps(adan)
