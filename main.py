d85229dfc2a24e755ea2f92f0d148848058cf4c9
azoz-moh <101649787+azoz-moh@users.noreply.github.com>
2022-03-19 15:11:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,15 +8,15 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-# origins = ["*"]
+origins = ["*"]
 
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=origins,
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 Machine_date = datetime.today().strftime('%d-%m')
